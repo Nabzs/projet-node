5748860a966f9faa0a8d95ad3cddc99706533fdf
Nabil <nabil.boukhorissa@gmail.com>
2023-03-13 10:27:51 +0100

    CORRECTIF BUG LIGNE 7

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ let port = 3000;
 
 app . listen(port, ( ) => {
 console.log( 'Le serveur est en route') ;
-console.log( `Serveur listening at http://
-localhost:${port}`) ;
+console.log( `Serveur listening at http://localhost:${port}`) ;
 } )
 app.get ( '/' , ( req , res , next) => {
 res.send ( 'Bonjour !') ;
